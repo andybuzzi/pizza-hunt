eb941b4ab2ffdba37beaaab3dd37dbc46644c350
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-11 12:02:54 -0500

    api routes added

---
+++ models/index.js
@@ -0,0 +1,5 @@
+//Like you did with Sequelize, you'll created the file index.js, in the models directory, that will package up all of the models. Even though currently you have just one, you should set yourself up for future additions.
+
+const Pizza = require("./Pizza");
+
+module.exports = { Pizza };
