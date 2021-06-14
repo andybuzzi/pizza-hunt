fda2c6567db0378a59a70dffa5b7ec415e68efee
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-06-14 15:42:41 -0500

    comment model added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //Like you did with Sequelize, you'll created the file index.js, in the models directory, that will package up all of the models. Even though currently you have just one, you should set yourself up for future additions.
 
 const Pizza = require("./Pizza");
+const Comment = require("./Comment");
 
-module.exports = { Pizza };
+module.exports = { Pizza, Comment };
